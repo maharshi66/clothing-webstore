ce9315f3a493913f264d89bac404822d8eee9632
Maharshi Shah <40323681+maharshi66@y.github.com>
2021-04-01 20:10:55 +0530

    Decomposed homepage into pages and components.Added scss styling to menu items

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './menu-item.style.scss'
+
+const MenuItem = ({title, imageUrl, size}) => {
+    return (
+        <div className={`${size} menu-item`}>
+            
+            <div 
+                style={{
+                    backgroundImage: `url(${imageUrl})`
+                }}
+                className='background-image'>
+            </div>
+
+            <div className='content'>
+                <h1 className='title'>{title.toUpperCase()}</h1>
+                <span className='subtitle'>Shop Now</span>
+            </div>
+        </div>
+    )
+}
+
+export default MenuItem;
