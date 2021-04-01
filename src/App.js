3884981cd961365b2ed506b6caa22e45663909f5
Maharshi Shah <40323681+maharshi66@y.github.com>
2021-04-02 01:28:43 +0530

    Added router and withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
-import HomePage from './pages/homepage/homepage.component'
+import { Switch, Route } from 'react-router-dom';
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path='/' component={HomePage}></Route>
     </div>
   );
 }
