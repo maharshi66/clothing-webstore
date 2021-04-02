20c5db23bb5422a7fe2c31160a5e8d5953635e51
Maharshi Shah <40323681+maharshi66@y.github.com>
2021-04-02 18:39:21 +0530

    Added logo to store and links to shop and contact

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -8,7 +8,7 @@ const CollectionItem = ({id, name, price, imageUrl}) => {
             </div>
             <div className='collection-footer'>
                 <span className='name'>{name}</span>
-                <span className='price'>${price}</span>
+                <span className='price'>&#8377;{price}</span>
             </div>
         </div>
     )
